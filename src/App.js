73f33149554a1fdf3bb0caa53a18f784177bbb1f
Hilal Aissani <hilalaissani@gmail.com>
2019-10-23 11:53:06 -0500

    sortable tree

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import 'react-sortable-tree/style.css';
+import SortableTree from 'react-sortable-tree';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+
+    const [file, setFile] = useState(null)
+    const [treeData, setTreeData] = useState([]);
+
+    const onDrop = useCallback(acceptedFiles => {
+
+        const reader = new FileReader()
+        acceptedFiles.forEach(file => reader.readAsArrayBuffer(file))
+
+        reader.onabort = () => console.log('file reading was aborted')
+        reader.onerror = () => console.log('file reading has failed')
+        reader.onload = () => {
+            // Do whatever you want with the file contents
+            let text = String.fromCharCode.apply(null, new Uint8Array(reader.result));
+            setFile(text);
+        }
+    }, []);
+
+
+    //////////////////////////////////////////////////////////////////////////////////
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    let arr = [
+
+        {
+            "title": "nothing ",
+            "subtitle": "nothing ",
+            "expanded": true,
+            "children": []
+        }
+    ];
+    if (file !== null) {
+        arr = JSON.parse(file);
+    }
+
+ console.log('arr ===> ', arr )
+    return (
+        <div>
+            <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+
+            <div style={{border: "2px solid black", height: 400, margin: 20}}>
+                <SortableTree
+                    treeData={arr}
+                    onChange={newTreeData => {
+                        console.log("newTreeData ==> ", newTreeData)
+                        try {
+                            setTreeData([{newTreeData}])
+                        } catch (err) {
+                            console.error("err  =>", err)
+                        }
+                    }
+                    }
+
+                />
+            </div>
+        </div>
+    )
 }
 
 export default App;
